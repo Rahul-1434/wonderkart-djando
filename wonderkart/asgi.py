755806d9152dc863a08adccc00133a8213f35de7
C. Rahul Reddy <crahulreddy4@gmail.com>
2025-06-23 13:12:46 +0530

    These is my first commit

---
+++ wonderkart/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for wonderkart project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wonderkart.settings')
+
+application = get_asgi_application()
