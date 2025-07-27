cc01ae36cdda61bb12dcc6f9dfafa871dae65569
C. Rahul Reddy <crahulreddy4@gmail.com>
2025-07-27 13:10:53 +0530

    dashboard & product images..

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from . models import Account
+from . models import Account,UserProfile
+from django.utils.html import format_html
 
 # Register your models here.
 class AccountAdmin(UserAdmin):
@@ -14,3 +15,12 @@ class AccountAdmin(UserAdmin):
     fieldsets=()
 
 admin.site.register(Account,AccountAdmin)
+
+class UserProfileAdmin(admin.ModelAdmin):
+    def thumbnail(self,object):
+        return format_html('<img src="{}" width="30" style="broder-radius:50%";>'.format(object.profile_picture.url))
+    thumbnail.short_description='Profile Picture'
+    list_display=('thumbnail','user','city','state','country')
+
+
+admin.site.register(UserProfile,UserProfileAdmin)
--- wonderkart/settings.py
+++ wonderkart/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!3%u)1&s_9o7q_2iy^fz(5$h_ni@)6vgl625l0$ym8e(5jkwe7'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',default=True,cast=bool) #True
 
 ALLOWED_HOSTS = ['*']
 
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
     'store',
     'carts',
     'orders',
+    'admin_honeypot',
 ]
 
 MIDDLEWARE = [
@@ -144,9 +146,9 @@ MESSAGE_TAGS={
     messages.ERROR: 'danger',
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'crahulreddy4@gmail.com'          # Your Gmail ID
-EMAIL_HOST_PASSWORD = 'bvcoxojpaxmszubj'           # Your App Password (no spaces)
+#SMTP configration
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT',cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')                   # Your Gmail ID
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')           # Your App Password (no spaces)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS',cast=bool)
